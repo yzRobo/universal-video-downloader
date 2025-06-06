97099626891cbfdea08fb0ecf618207662c9d649
robomedia <robomediatv@gmail.com>
2025-06-06 15:19:16 -0400

    V2 Updates
    Finalize features for v2.0 release

--- start-server.sh
+++ start-server.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 # A script to install dependencies and start the Vimeo Downloader server.
 
-echo "Checking for dependencies..."
-
-# Check if node_modules directory exists. If not, run npm install.
-if [ ! -d "node_modules" ]; then
-  echo "Dependencies not found. Running npm install..."
-  npm install
-  if [ $? -ne 0 ]; then
-    echo "npm install failed. Please check for errors."
-    exit 1
-  fi
+echo "Installing/Verifying dependencies..."
+npm install
+if [ $? -ne 0 ]; then
+  echo "npm install failed. Please check for errors."
+  exit 1
 fi
 
 echo ""
